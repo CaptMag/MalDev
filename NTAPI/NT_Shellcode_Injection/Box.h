7d0296eb7a258fc47e2c843caa410335a8686d5b
CaptMag <77859060+CaptMag@users.noreply.github.com>
2025-10-01 13:45:12 -0400

    Added Header Files

---
+++ NTAPI/NT_Shellcode_Injection/Box.h
@@ -0,0 +1,140 @@
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+#include <wincrypt.h>
+
+
+
+#ifndef STATUS_SUCCESS
+#define STATUS_SUCCESS ((NTSTATUS)0x00000000L)
+#define OKAY(MSG, ...) printf("[+] "          MSG "\n", ##__VA_ARGS__)
+#define INFO(MSG, ...) printf("[*] "          MSG "\n", ##__VA_ARGS__)
+#define WARN(MSG, ...) fprintf(stderr, "[-] " MSG "\n", ##__VA_ARGS__)
+#endif
+
+/* If winternl.h isn't available, define CLIENT_ID and OBJECT_ATTRIBUTES minimal types */
+#ifndef NT_SUCCESS
+#define NT_SUCCESS(Status) (((NTSTATUS)(Status)) >= 0)
+#endif
+
+
+typedef struct _UNICODE_STRING
+{
+    USHORT Length;
+    USHORT MaximumLength;
+    _Field_size_bytes_part_opt_(MaximumLength, Length) PWCH Buffer;
+} UNICODE_STRING, * PUNICODE_STRING;
+
+typedef struct _OBJECT_ATTRIBUTES
+{
+    ULONG Length;
+    HANDLE RootDirectory;
+    PUNICODE_STRING ObjectName;
+    ULONG Attributes;
+    PVOID SecurityDescriptor; // PSECURITY_DESCRIPTOR;
+    PVOID SecurityQualityOfService; // PSECURITY_QUALITY_OF_SERVICE
+} OBJECT_ATTRIBUTES, * POBJECT_ATTRIBUTES;
+
+
+typedef struct _CLIENT_ID
+{
+    HANDLE UniqueProcess;
+    HANDLE UniqueThread;
+} CLIENT_ID, * PCLIENT_ID;
+
+typedef struct _PS_ATTRIBUTE {
+    ULONG  Attribute;
+    SIZE_T Size;
+    union
+    {
+        ULONG Value;
+        PVOID ValuePtr;
+    } u1;
+    PSIZE_T ReturnLength;
+} PS_ATTRIBUTE, * PPS_ATTRIBUTE;
+
+
+typedef struct _PS_ATTRIBUTE_LIST {
+    SIZE_T       TotalLength;
+    PS_ATTRIBUTE Attributes[1];
+} PS_ATTRIBUTE_LIST, * PPS_ATTRIBUTE_LIST;
+
+
+typedef NTSTATUS(NTAPI* fn_NtOpenProcess)(
+    _Out_ PHANDLE ProcessHandle,
+    _In_ ACCESS_MASK DesiredAccess,
+    _In_ POBJECT_ATTRIBUTES ObjectAttributes,
+    _In_opt_ PCLIENT_ID ClientId
+    );
+
+typedef NTSTATUS(NTAPI* fn_NtCreateThreadEx)(
+    OUT PHANDLE ThreadHandle,
+    IN ACCESS_MASK DesiredAccess,
+    IN POBJECT_ATTRIBUTES ObjectAttributes OPTIONAL,
+    IN HANDLE ProcessHandle,
+    IN PVOID StartRoutine,
+    IN PVOID Argument OPTIONAL,
+    IN ULONG CreateFlags,
+    IN SIZE_T ZeroBits,
+    IN SIZE_T StackSize,
+    IN SIZE_T MaximumStackSize,
+    IN PPS_ATTRIBUTE_LIST AttributeList OPTIONAL
+    );
+
+typedef NTSTATUS(NTAPI* fn_NtAllocateVirtualMemory)(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    ULONG_PTR ZeroBits,
+    PSIZE_T RegionSize,
+    ULONG AllocationType,
+    ULONG Protect
+    );
+
+typedef NTSTATUS(NTAPI* fn_NtWriteVirtualMemory)(
+    IN HANDLE ProcessHandle,
+    IN PVOID BaseAddress,
+    IN PVOID Buffer,
+    IN SIZE_T NumberOfBytesToWrite,
+    OUT PSIZE_T NumberOfBytesWritten OPTIONAL
+    );
+
+typedef NTSTATUS(NTAPI* fn_NtProtectVirtualMemory)(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T NumberOfBytesToProtect,
+    ULONG NewProtect,
+    PULONG OldProtect
+    );
+
+
+typedef NTSTATUS(NTAPI* PFN_NtFreeVirtualMemory)(
+    HANDLE ProcessHandle,
+    PVOID* BaseAddress,
+    PSIZE_T RegionSize,
+    ULONG FreeType
+    );
+
+
+typedef NTSTATUS(WINAPI* _SystemFunction033)(
+    struct ustring* memoryRegion,
+    struct ustring* keyPointer);
+
+struct ustring {
+    DWORD Length;
+    DWORD MaximumLength;
+    PUCHAR Buffer;
+} data, key;
+
+
+BOOL EncryptRC4(
+    _In_ CONST PBYTE pShellcode,
+    _In_ CONST SIZE_T sSizeofShellcode
+);
+
+BOOL NtShellInjection(
+    _In_ CONST DWORD PID,
+    CONST PBYTE pEncryptedShellcode,
+    _In_ CONST PBYTE pShellcode,
+    _In_ CONST SIZE_T sSizeofShellcode
+);
