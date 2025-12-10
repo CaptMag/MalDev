3ecdf2fdd846a5884d5b5181f3891a762f89eb0e
CaptMag <DigitalColdWar1@gmail.com>
2025-12-10 14:17:02 -0500

    Added Fiber Injection

---
+++ Process_Injection/Fiber_Injection/util.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <Windows.h>
+#include <stdio.h>
+#include <tlhelp32.h>
+
+#define OKAY(MSG, ...) printf("[+] "		  MSG "\n", ##__VA_ARGS__)
+#define INFO(MSG, ...) printf("[*] "          MSG "\n", ##__VA_ARGS__)
+#define WARN(MSG, ...) fprintf(stderr, "[-] " MSG "\n", ##__VA_ARGS__)
+#define PRINT_ERROR(MSG, ...) fprintf(stderr, "[!] " MSG "Failed! Error: 0x%lx""\n", GetLastError())
